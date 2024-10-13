0da7bedbcb3c5e818ce6c31cf07abe522b410566
Sajant Anand <sajantanand@aol.com>
2024-10-12 18:37:41 -0700

    Exponential Spin Model for TFI

--- tenpy/models/spins.py
+++ tenpy/models/spins.py
@@ -12,7 +12,9 @@ from .model import CouplingMPOModel, NearestNeighborModel
 from .lattice import Chain, Square
 from ..tools.params import asConfig
 
-__all__ = ['SpinModel', 'SpinChain', 'XXXChain', 'ExponentiallyDecayingXXZ', 'AnisotropicSpinModel', 'AnisotropicBarberPole', 'ExponentiallyDecayingXX']
+__all__ = ['SpinModel', 'SpinChain', 'XXXChain', 
+        'AnisotropicSpinModel', 'AnisotropicBarberPole', 
+        'ExponentiallyDecayingXXZ', 'ExponentiallyDecayingXX', 'ExponentiallyDecayingXY', 'ExponentiallyDecayingSpinModel']
 
 
 class SpinModel(CouplingMPOModel):
@@ -413,3 +415,135 @@ class ExponentiallyDecayingXX(CouplingMPOModel):
             self.add_exponentially_decaying_coupling(pre*4, lam, 'Sx', 'Sx')
         # done
 
+class ExponentiallyDecayingXY(CouplingMPOModel):
+    """
+    f(r) * [X_i X_{i+r} + Y_i Y_{i+r}]
+    f(r) is approximated by a set of exponentials
+    bond dimension is num_exponentials + 2
+
+    One needs to approximate f(r) prior to initialization of the model.
+
+    This is used for Floquet XXZ evolution. No conservation is allowed.
+    """
+
+    def init_sites(self, model_params):
+        conserve = model_params.get('conserve', None)
+        #assert conserve == None, "Nothing preserved in Floquet model."
+
+        sort_charge = model_params.get('sort_charge', None)
+        S = model_params.get('S', 0.5)
+
+        site = SpinSite(S=S, conserve=conserve, sort_charge=sort_charge)
+        return site
+
+    def init_terms(self, model_params):
+        lambdas = model_params['lambdas']
+        prefactors = model_params['prefactors']
+
+        for lam, pre in zip(lambdas, prefactors):
+            # Sp = Sx + i Sy, Sm = Sx - i Sy,  Sx = (Sp+Sm)/2, Sy = (Sp-Sm)/2i
+            # Sp Sm + Sm Sp = 2 (Sx Sx + Sy Sy)
+            # self.add_exponentially_decaying_coupling(pre*2, lam, 'Sp', 'Sm', plus_hc=True)
+            self.add_exponentially_decaying_coupling(pre*4, lam, 'Sx', 'Sx')
+            self.add_exponentially_decaying_coupling(pre*4, lam, 'Sy', 'Sy')
+        # done
+
+class ExponentiallyDecayingSpinModel(CouplingMPOModel):
+    r"""Spin-S sites coupled by nearest neighbor interactions.
+
+    The Hamiltonian reads:
+
+    .. math ::
+        H = \sum_{i < j}
+              f(|i-j|) (\mathtt{Jx} S^x_i S^x_j + \mathtt{Jy} S^y_i S^y_j + \mathtt{Jz} S^z_i S^z_j
+            + \mathtt{muJ} i/2 (S^{-}_i S^{+}_j - S^{+}_i S^{-}_j))  \\
+            - \sum_i (\mathtt{hx} S^x_i + \mathtt{hy} S^y_i + \mathtt{hz} S^z_i) \\
+            + \sum_i (\mathtt{D} (S^z_i)^2 + \mathtt{E} ((S^x_i)^2 - (S^y_i)^2))
+
+    Here, :math:`i< j` denotes all pairs of sites and :math:`f(|i-j|)` is an interaction
+    that depends on the distance between the sites.
+    All parameters are collected in a single dictionary `model_params`, which
+    is turned into a :class:`~tenpy.tools.params.Config` object.
+
+    Parameters
+    ----------
+    model_params : :class:`~tenpy.tools.params.Config`
+        Parameters for the model. See :cfg:config:`SpinModel` below.
+
+    Options
+    -------
+    .. cfg:config :: SpinModel
+        :include: CouplingMPOModel
+
+        S : {0.5, 1, 1.5, 2, ...}
+            The 2S+1 local states range from m = -S, -S+1, ... +S.
+        conserve : 'best' | 'Sz' | 'parity' | None
+            What should be conserved. See :class:`~tenpy.networks.Site.SpinSite`.
+            For ``'best'``, we check the parameters what can be preserved.
+        sort_charge : bool
+            Whether to sort by charges of physical legs. `True` by default.
+        Jx, Jy, Jz, hx, hy, hz, muJ, D, E  : float | array
+            Coupling as defined for the Hamiltonian above.
+
+    """
+    def init_sites(self, model_params):
+        S = model_params.get('S', 0.5, 'real')
+        conserve = model_params.get('conserve', 'best', str)
+        if conserve == 'best':
+            # check how much we can conserve
+            if not model_params.any_nonzero([('Jx', 'Jy'), 'hx', 'hy', 'E'],
+                                            "check Sz conservation"):
+                conserve = 'Sz'
+            elif not model_params.any_nonzero(['hx', 'hy'], "check parity conservation"):
+                conserve = 'parity'
+            else:
+                conserve = None
+            self.logger.info("%s: set conserve to %s", self.name, conserve)
+        sort_charge = model_params.get('sort_charge', True, bool)
+        site = SpinSite(S, conserve, sort_charge)
+        return site
+
+    def init_terms(self, model_params):
+        Jx = model_params.get('Jx', 1., 'real_or_array')
+        Jy = model_params.get('Jy', 1., 'real_or_array')
+        Jz = model_params.get('Jz', 1., 'real_or_array')
+        hx = model_params.get('hx', 0., 'real_or_array')
+        hy = model_params.get('hy', 0., 'real_or_array')
+        hz = model_params.get('hz', 0., 'real_or_array')
+        D = model_params.get('D', 0., 'real_or_array')
+        E = model_params.get('E', 0., 'real_or_array')
+        muJ = model_params.get('muJ', 0., 'real_or_array')
+
+        lambdas = model_params['lambdas']
+        prefactors = model_params['prefactors']
+        
+        print(Jx, Jy, Jz, hx, hy, hz, D, E, muJ)
+
+        # (u is always 0 as we have only one site in the unit cell)
+        for u in range(len(self.lat.unit_cell)):
+            self.add_onsite(-hx, u, 'Sx')
+            self.add_onsite(-hy, u, 'Sy')
+            self.add_onsite(-hz, u, 'Sz')
+            self.add_onsite(D, u, 'Sz Sz')
+            self.add_onsite(E * 0.5, u, 'Sp Sp')
+            self.add_onsite(E * 0.5, u, 'Sm Sm')
+        # Sp = Sx + i Sy, Sm = Sx - i Sy,  Sx = (Sp+Sm)/2, Sy = (Sp-Sm)/2i
+        # Sx.Sx = 0.25 ( Sp.Sm + Sm.Sp + Sp.Sp + Sm.Sm )
+        # Sy.Sy = 0.25 ( Sp.Sm + Sm.Sp - Sp.Sp - Sm.Sm )
+        for lam, pre in zip(lambdas, prefactors):
+            # Only include terms if the prefactors are non-zero; else we just add
+            # unnecessary states (bond dimension) to the FSM (MPO).
+
+            # Sp = Sx + i Sy, Sm = Sx - i Sy,  Sx = (Sp+Sm)/2, Sy = (Sp-Sm)/2i
+            # Sp Sm + Sm Sp = 2 (Sx Sx + Sy Sy)
+            if Jx + Jy != 0.0:
+                self.add_exponentially_decaying_coupling(pre * (Jx + Jy) / 4., lam, 'Sp', 'Sm', plus_hc=True)
+            if Jx - Jy != 0.0:
+                self.add_exponentially_decaying_coupling(pre * (Jx - Jy) / 4., lam, 'Sp', 'Sp', plus_hc=True)
+            if muJ != 0.0:
+                self.add_exponentially_decaying_coupling(pre * muJ * 0.5j, lam, 'Sm', 'Sp', plus_hc=True)
+                # Somehow this below line, which appears to be the same as the one above, causes an error?
+                # Says the function doesn't have a "plus_hc" argument.
+                #self.add_exponentially_decaying_coupling(pre * muJ * 0.5j, lam, 'Sm', 'Sp', pluc_hc=True)
+            if Jz != 0:
+                self.add_exponentially_decaying_coupling(pre * Jz, lam, 'Sz', 'Sz')
