0baa02c7ddd231a30269cd1fc1bcd1ec728d2f02
Sajant Anand <sajantanand@aol.com>
2025-09-08 11:04:12 -0700

    HOCC with U(1) conservation

--- tenpy/models/spins.py
+++ tenpy/models/spins.py
@@ -11,6 +11,7 @@ from ..networks.site import SpinSite
 from .model import CouplingMPOModel, NearestNeighborModel
 from .lattice import Chain, Square
 from ..tools.params import asConfig
+from ..linalg.charges import LegCharge, ChargeInfo
 
 __all__ = ['SpinModel', 'SpinChain', 'XXXChain', 
         'AnisotropicSpinModel', 'AnisotropicBarberPole', 'FiniteRangeSpinModel',
@@ -109,8 +110,8 @@ class SpinChain(SpinModel, NearestNeighborModel):
     default_lattice = Chain
     force_default_lattice = True
 
-class XXXChain(SpinChain):
-    """The :class:`SpinChain` for the XXX model with conserved MPOs.
+class XXXChain(SpinModel):
+    """The :class:`SpinModel` for the XXX model with conserved MPOs.
 
     We allow for both U(1) charge conserving and non charge conserving MPOs.
 
@@ -118,10 +119,15 @@ class XXXChain(SpinChain):
     us the range of the conserved charge, and we place k MPO tensors centered on site
     center.
 
+    Note: This does NOT inherit from NearestNeighborModel, so we cannot do TEBD.
+
     See the :class:`SpinModel` for the documentation of parameters.
     """
     
-    def conserved_charge_MPO_U1(self, k, center=None, verbose=False):
+    default_lattice = Chain
+    force_default_lattice = True
+    
+    def conserved_charge_MPO_U1(self, k, center=None, verbose=False, staggered=False):
         assert k >= 1, "Conserved quantities for k>=1."
         if not hasattr(self, 'MPOs_U1'):
             self.MPOs_U1 = {}
@@ -133,28 +139,31 @@ class XXXChain(SpinChain):
             assert k > 1, "Can't make MPO of L=1."
             assert center - k // 2 >= 0
             assert center + (k // 2 - (k+1) % 2) <= L-1
-        if k in self.MPOs_U1.keys() and center == None and not verbose:
+        if k in self.MPOs_U1.keys() and center == None and not verbose and not staggered:
             return self.MPOs_U1[k]
 
         site = sites[0] # This contains the operators we use to build the sites.
         Sp = site.get_op('Sp')
         Sm = site.get_op('Sm')
         Sz = site.get_op('Sz')
-        Zero = 0 * Sz
         I = site.get_op('Id')
+        Zero = None
+
         H_MPO = self.H_MPO
+
+        D = 3*k - 1
+
+        chinfo = I.chinfo
+        triv_charge = chinfo == ChargeInfo()
+        wR_charges = np.zeros((D, 1)) if not triv_charge else np.zeros((D, 0))
+        wR_slices = np.arange(D+1)
 
         if k in self.Tensors_U1.keys() and not verbose:
             dW, dI = self.Tensors_U1[k]
         else:
-            D = 3*k - 1
             dI = np.empty((D, D), dtype=object)
             dW = np.empty((D, D), dtype=object)
             dW_str = np.empty((D, D), dtype=object)
-            for i in range(D):
-                for j in range(D):
-                    dI[i,j] = Zero
-                    dW[i,j] = Zero
 
             dI[0, 0] = dI[D-1, D-1] = I
             dW[0, 0] = dW[D-1, D-1] = I
@@ -169,7 +178,7 @@ class XXXChain(SpinChain):
                 sigma_str = ['sqrt(2).Sm', 'sqrt(2).Sp', '2.Sz']
                 sigma_dag_str = ['sqrt(2).Sp', 'sqrt(2).Sm', '2.Sz']
                 M = [[-2*Sz*1.j, Zero, np.sqrt(2)*Sm*1.j], [Zero, 2*Sz*1.j, -np.sqrt(2)*Sp*1.j], [np.sqrt(2)*Sp*1.j, -np.sqrt(2)*Sm*1.j, Zero]]
-                M_str = [['-2i.Sz', '0', 'i.sqrt(2).Sm'], ['0', '2i.Sz', '-i.sqrt(2).Sp'], ['i.sqrt(2).Sp', '-i.sqrt(2).Sm', '0']]
+                M_str = [['-2i.Sz', 'None', 'i.sqrt(2).Sm'], ['None', '2i.Sz', '-i.sqrt(2).Sp'], ['i.sqrt(2).Sp', '-i.sqrt(2).Sm', '0']]
 
                 def Catalan(n):
                     return comb(2*n, n) - comb(2*n, n-1)
@@ -178,7 +187,7 @@ class XXXChain(SpinChain):
                 dW[D-4:D-1, D-1] = sigma
                 dW_str[0, 1:4] = sigma_dag_str
                 dW_str[D-4:D-1, D-1] = sigma_str
-
+                
                 assert (D - 1 - 3 - 1) % 3 == 0
                 for i in range(int((D - 1 - 3 - 1)//3)):
                     dW[(1+3*i):(1+3*(i+1)), (4+3*i):(4+3*(i+1))] = M
@@ -198,12 +207,30 @@ class XXXChain(SpinChain):
                 print(dW_str)
             self.Tensors_U1[k] = (dW, dI)
 
+        if staggered:
+            dWL = dW.copy()
+            dWR = dW.copy()
+
+            for i in range(1,D):
+                dWR[0,i] = -1 * dWR[0,i] if dWR[0,i] is not None else None
+
+        if not triv_charge:
+            wR_charges[:,0] = [0] + [2,-2,0] * (k-1) + [0]
+        wR_leg = LegCharge(chinfo, wR_slices, wR_charges, qconj=-1)
+
         IdL = [0] * (L + 1)
         IdR = [-1] * (L + 1)
         from ..networks.mpo import MPO
         if center == None:
-            CC_MPO = MPO.from_grids(sites, [dW]*L, H_MPO.bc, IdL, IdR, max_range=k, explicit_plus_hc=False)
-            self.MPOs_U1[k] = CC_MPO
+            # We need to provide the leg charges since the there are dangling terms that do not connect to IdL or IdR.
+            # We could get rid of these terms by cutting of rownd and columns of the left and right matrices, but since
+            # the interactions are non-local (beyond NN), we'd have to change multiple tensors. It's easier to just
+            # define the charges.
+            if staggered:
+                CC_MPO = MPO.from_grids(sites, [dWL]*(L//2) + [dWR]*(L-L//2), H_MPO.bc, IdL, IdR, legs = [wR_leg.conj()] * (L+1), max_range=k, explicit_plus_hc=False)
+            else:
+                CC_MPO = MPO.from_grids(sites, [dW]*(L), H_MPO.bc, IdL, IdR, legs = [wR_leg.conj()] * (L+1), max_range=k, explicit_plus_hc=False)
+                self.MPOs_U1[k] = CC_MPO
         else:
             CC_MPO = MPO.from_grids(list(sites[:k]), [dW]*k, H_MPO.bc, IdL[:k+1], IdR[:k+1], max_range=k, explicit_plus_hc=False)
             I = I.add_trivial_leg(axis=0, label='wL', qconj=1).add_trivial_leg(axis=1, label='wR', qconj=-1)
@@ -211,7 +238,7 @@ class XXXChain(SpinChain):
 
         return CC_MPO
 
-    def conserved_charge_MPO_trivial(self, k, center=None, verbose=False):
+    def conserved_charge_MPO_trivial(self, k, center=None, verbose=False, staggered=False):
         assert k >= 1, "Conserved quantities for k>=1."
         if not hasattr(self, 'MPOs'):
             self.MPOs = {}
@@ -223,28 +250,31 @@ class XXXChain(SpinChain):
             assert k > 1, "Can't make MPO of L=1."
             assert center - k // 2 >= 0
             assert center + (k // 2 - (k+1) % 2) <= L-1
-        if k in self.MPOs.keys() and center == None and not verbose:
+        if k in self.MPOs.keys() and center == None and not verbose and not staggered:
             return self.MPOs[k]
 
         site = sites[0] # This contains the operators we use to build the sites.
         X = 2*site.get_op('Sx')
         Y = 2*site.get_op('Sy')
         Z = 2*site.get_op('Sz')
-        Zero = 0 * Z
         I = site.get_op('Id')
+        Zero = None
+    
         H_MPO = self.H_MPO
+
+        D = 3*k - 1
+
+        chinfo = I.chinfo
+        assert chinfo == ChargeInfo()
+        wR_charges = np.zeros((D, 0))
+        wR_slices = np.arange(D+1)
 
         if k in self.Tensors.keys() and not verbose:
             dW, dI = self.Tensors[k]
         else:
-            D = 3*k - 1
             dI = np.empty((D, D), dtype=object)
             dW = np.empty((D, D), dtype=object)
             dW_str = np.empty((D, D), dtype=object)
-            for i in range(D):
-                for j in range(D):
-                    dI[i,j] = Zero
-                    dW[i,j] = Zero
 
             dI[0, 0] = dI[D-1, D-1] = I
             dW[0, 0] = dW[D-1, D-1] = I
@@ -279,6 +309,7 @@ class XXXChain(SpinChain):
                             Cn_str = 'C' + str(n)
                             I3 = [[Cn*I, Zero, Zero], [Zero, Cn*I, Zero], [Zero, Zero, Cn*I]]
                             I3_str = [[Cn_str, '0', '0'], ['0', Cn_str, '0'], ['0', '0', Cn_str]]
+
                             dW[(1+3*(i-1)):(1+3*i), (4+3*(j-1)):(4+3*j)] = I3
                             dW_str[(1+3*(i-1)):(1+3*i), (4+3*(j-1)):(4+3*j)] = I3_str
 
@@ -286,14 +317,25 @@ class XXXChain(SpinChain):
                 print(dW_str)
             self.Tensors[k] = (dW, dI)
 
+        if staggered:
+            dWL = dW.copy()
+            dWR = dW.copy()
+
+            for i in range(1, D):
+                dWR[0,i] = -1 * dWR[0,i] if dWR[0,i] is not None else None
+
+        wR_leg = LegCharge(chinfo, wR_slices, wR_charges, qconj=-1)
+
         IdL = [0] * (L + 1)
         IdR = [-1] * (L + 1)
         from ..networks.mpo import MPO
         if center == None:
-            CC_MPO = MPO.from_grids(sites, [dW]*L, H_MPO.bc, IdL, IdR, max_range=k, explicit_plus_hc=False)
-            self.MPOs[k] = CC_MPO
+            if staggered:
+                CC_MPO = MPO.from_grids(sites, [dWL]*(L//2) + [dWR]*(L-L//2), H_MPO.bc, IdL, IdR, legs = [wR_leg.conj()] * (L+1), max_range=k, explicit_plus_hc=False)
+            else:
+                CC_MPO = MPO.from_grids(sites, [dW]*(L), H_MPO.bc, IdL, IdR, legs = [wR_leg.conj()] * (L+1), max_range=k, explicit_plus_hc=False)
+                self.MPOs[k] = CC_MPO
         else:
-            #CC_MPO = MPO.from_grids(sites, [dI]*(center - k//2) + [dW]*k + [dI]*(L-center + k//2 - k), H_MPO.bc, IdL, IdR, max_range=k, explicit_plus_hc=False)
             CC_MPO = MPO.from_grids(list(sites[:k]), [dW]*k, H_MPO.bc, IdL[:k+1], IdR[:k+1], max_range=k, explicit_plus_hc=False)
             I = I.add_trivial_leg(axis=0, label='wL', qconj=1).add_trivial_leg(axis=1, label='wR', qconj=-1)
             CC_MPO = MPO(sites, [I]*(center - k//2) + CC_MPO._W + [I]*(L-center + k//2 - k), bc=H_MPO.bc, IdL=IdL, IdR=IdR, max_range=k, explicit_plus_hc=False)
